f8d3cd54c07471954b9f5c81ed2af4eeb4ac312c
JackR42 <j.roumimper@gmail.com>
2022-10-25 18:58:54 +0200

    Update dba42.sql

--- dba42.sql
+++ dba42.sql
@@ -1,7 +1,7 @@
 create table t42(c1 int primary key, ç2 varchar(32)
 go
-insert into t42 value(1, 'Project 07');
-insert into t42 value(1, 'Project 42');
+insert into t42 values(1, 'Project 07');
+insert into t42 values(2, 'Project 42');
 go
 select * from t42
 go
