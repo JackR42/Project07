04eca654cb78f3330e74fb03bccfa6ca80e7a517
JackR42 <j.roumimper@gmail.com>
2022-10-25 18:42:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,12 @@ resource "azurerm_sql_database" "project07" {
   location            = azurerm_resource_group.project07.location
   server_name         = azurerm_mssql_server.project07.name
 }
+
+# Create FW rule to allow access from own IP address to the SQL Instance
+resource "azurerm_sql_firewall_rule" "project07" {
+  name                = "FirewallRule1"
+  resource_group_name = azurerm_resource_group.project07.name
+  server_name         = azurerm_mssql_server.project07.name
+  start_ip_address    = "91.205.194.1"
+  end_ip_address      = "91.205.194.1"
+}
