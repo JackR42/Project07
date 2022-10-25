2e1366b1d5e5e5acc6f805d2dd0c3728b66a9f8e
JackR42 <j.roumimper@gmail.com>
2022-10-25 18:37:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,9 @@ resource "azurerm_mssql_server" "project07" {
   administrator_login_password = "ABCabc123.42"
 }
 
+resource "azurerm_sql_database" "project07" {
+  name                = "dba42"
+  resource_group_name = azurerm_resource_group.project07.name
+  location            = azurerm_resource_group.project07.location
+  server_name         = azurerm_mssql_server.project07.name
+}
