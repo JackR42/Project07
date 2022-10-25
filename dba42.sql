310649a074117c61018d211df39a8a02d67e673f
JackR42 <j.roumimper@gmail.com>
2022-10-25 19:01:55 +0200

    Update dba42.sql

--- dba42.sql
+++ dba42.sql
@@ -1,4 +1,6 @@
-create table t42(c1 int primary key, ç2 varchar(32)
+--drop table if exists t42;
+go
+create table t42(c1 int primary key, c2 varchar(32));
 go
 insert into t42 values(1, 'Project 07');
 insert into t42 values(2, 'Project 42');
